69d100dbfaef159d295b46905f4468e24e174d38
SumerThakur <thakursumer2000@gmail.com>
2020-10-04 22:30:29 +0530

    Hey! This is my first pull request on Github. Here are some minor twerks and obviously some easter eggs. Merge it if you like it. Peace!

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-$(function() {
-    $("#shrekTrigger").mouseover(() => {
-        $("#shrek").animate({
-            opacity: 1
-        });
+$(function () {
+  $("#shrekTrigger").mouseover(() => {
+    $("#shrek").animate({
+      opacity: 1,
     });
+    $("#EasterEggs").animate({
+      opacity: 1,
+    });
+  });
 
-    $("#shrekTrigger").mouseout(() => {
-        $("#shrek").animate({
-            opacity: 0
-        });
+  $("#shrekTrigger").mouseout(() => {
+    $("#shrek").animate({
+      opacity: 0,
     });
+    $("#EasterEggs").animate({
+      opacity: 0,
+    });
+  });
 });
