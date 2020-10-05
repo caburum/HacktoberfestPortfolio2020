58b6cd6b488d024ce56f93efcd59586d9683f127
gonzalojs <eogutier@gmail.com>
2020-10-04 21:25:10 -0300

    konami code

---
+++ konamiCode.js
@@ -0,0 +1,24 @@
+let KonamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var myAudio = new Audio('./audio/Konami Intro - Metal Gear Solid ᴴᴰ.mp3');
+
+
+
+const detectCode = (code) => {
+  let input = ''
+  let codeToDetect = code.join('')
+  console.log(codeToDetect)
+  // detect key events
+  document.addEventListener('keydown', e => {
+
+    //add events to input
+    input += ('' + e.key)
+    if (input === codeToDetect) {
+      myAudio.play()
+    }
+    // reset the code
+    if (!codeToDetect.indexOf(input)) return;
+    input = ('' + e.key)
+  })
+}
+
+detectCode(KonamiCode)
